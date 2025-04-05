4afeeb88bc2c9548ff4faf5a3e0c1158c392bffb
UpstairsForest <nikita.batalov@uzh.ch>
2025-04-05 17:30:04 +0200

    add template recipes

---
+++ src/kernel/recipes.ts
@@ -0,0 +1,98 @@
+export const recipes = [
+    {
+        "name": "Smart Plating",
+        "prod_rate": 2,
+        "inputs": [
+            {
+                "name": "Rotor",
+                "cons_rate": 2
+            },
+            {
+                "name": "Reinforced Iron Plate",
+                "cons_rate": 2
+            }
+        ]
+    },
+    {
+        "name": "Modular Frame",
+        "prod_rate": 5,
+        "inputs": [
+            {
+                "name": "Screw",
+                "cons_rate": 140
+            },
+            {
+                "name": "Reinforced Iron Plate",
+                "cons_rate": 7.5
+            }
+        ]
+    },
+    {
+        "name": "Rotor",
+        "prod_rate": 4,
+        "inputs": [
+            {
+                "name": "Screw",
+                "cons_rate": 100
+            },
+            {
+                "name": "Iron Rod",
+                "cons_rate": 20
+            }
+        ]
+    },
+    {
+        "name": "Reinforced Iron Plate",
+        "prod_rate": 15,
+        "inputs": [
+            {
+                "name": "Screw",
+                "cons_rate": 250
+            },
+            {
+                "name": "Iron Plate",
+                "cons_rate": 90
+            }
+        ]
+    },
+    {
+        "name": "Screw",
+        "prod_rate": 40,
+        "inputs": [
+            {
+                "name": "Iron Rod",
+                "cons_rate": 10
+            }
+        ]
+    },
+    {
+        "name": "Iron Rod",
+        "prod_rate": 15,
+        "inputs": [
+            {
+                "name": "Iron Ingot",
+                "cons_rate": 15
+            }
+        ]
+    },
+    {
+        "name": "Iron Plate",
+        "prod_rate": 20,
+        "inputs": [
+            {
+                "name": "Iron Ingot",
+                "cons_rate": 30
+            }
+        ]
+    },
+    {
+        "name": "Iron Ingot",
+        "prod_rate": 120,
+        "inputs": [
+            {
+                "name": "Iron Ore",
+                "cons_rate": 120
+            }
+        ]
+    }
+]
